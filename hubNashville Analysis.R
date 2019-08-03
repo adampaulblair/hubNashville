d3c9ee442ae4088339a66a07024bae585a817118
adampaulblair <adampaulblair@gmail.com>
2019-08-03 16:05:16 -0500

    Calculated averages and started appending census tract IDs

--- hubNashville Analysis.R
+++ hubNashville Analysis.R
@@ -1,8 +1,10 @@
 ## install.packages("RSocrata")
 ## install.packages("gmodels")
+## install.packages("tigris")
 
-library("RSocrata")
-library("gmodels")
+library(RSocrata)
+library(gmodels)
+library(tigris)
 
 ## Import data using API
 
@@ -37,6 +39,25 @@ case_request <- factor(case_request)
 case_subrequest <- factor(case_subrequest)
 incident_council_district <- factor(incident_council_district)
 
+## Specify numeric variables
+
+latitude <- as.numeric(latitude)
+longitude <- as.numeric(longitude)
+
+## Calculate and print average days open by case and subcase
+
+case_means <- aggregate(df, by = list(Group.case_request = case_request), FUN = mean, na.rm = TRUE)
+
+case_means[c("Group.case_request", "days_open")]
+
+## Append census tract IDs using lat-long
+
+coords <- data.frame(latitude, longitude)
+
+coords$tract_id <- apply(coords, 1, function(row) call_geolocator_latlon(row["latitude"], row["longitude"]))
+
 detach(df)
 
 ## Save file
+
+## write.csv(df, file = "C:/Users/adamp/Dropbox/TSU/Research/hubNashville/hubNashville/df.txt")
